cff9e44734487157d959a4e7f25e93e2796ef86a
Sam Gould <drmcman67@yahoo.com>
2018-10-04 13:16:00 -0400

    Flock outline

---
+++ Assets/Scripts/LeaderFlock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderFlock : MonoBehaviour {
+    public GameObject[] flock;
+    public Vector2 strength1;
+    public Vector2 strength2;
+    public Vector2 strength3;
+
+	// Use this for initialization
+	void Start () {
+        flock = GameObject.FindGameObjectsWithTag("BOID");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject a in flock) {
+            strength1 = avoid_collisions(a);
+            strength2 = match_velocity(a);
+            strength3 = flock_to_center(a);
+        }
+	}
+    public Vector2 avoid_collisions(GameObject b) {
+        return new Vector2(0,0);
+
+    }
+    public Vector2 match_velocity(GameObject b)
+    {
+        return new Vector2(0, 0);
+
+    }
+    public Vector2 flock_to_center(GameObject b)
+    {
+        return new Vector2(0, 0);
+
+    }
+}
