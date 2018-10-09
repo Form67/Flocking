1b21c6ccb1f6b3347b9579ccce23c9a9906bb0cf
Jared Okun <okunj20@gmail.com>
2018-10-09 01:58:14 -0400

    just need to implement align

--- Assets/Scripts/LeaderFlock.cs
+++ Assets/Scripts/LeaderFlock.cs
@@ -4,41 +4,62 @@ using UnityEngine;
 
 public class LeaderFlock : MonoBehaviour {
     public GameObject[] flock;
-    public Vector2 strength1;
-    public Vector2 strength2;
-    public Vector2 strength3;
+	Vector2 instantaneousVelocity;
 
 	public float seperationConstant;
+	public float closeEnoughDistance;
 	public float maxSpeed; //Could be changed to maxAcceleration
 	// Use this for initialization
 	void Start () {
         flock = GameObject.FindGameObjectsWithTag("BOID");
+		instantaneousVelocity = Vector2.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         foreach (GameObject a in flock) {
-            strength1 = avoid_collisions(a);
-            strength2 = match_velocity(a);
-            strength3 = flock_to_center(a);
-            a.GetComponent<Rigidbody2D>().velocity = a.GetComponent<Rigidbody2D>().velocity + strength1 + strength2 + strength3;
+            Vector2 strength1 = avoid_collisions(a);
+            Vector2 strength2 = match_velocity(a);
+            Vector2 strength3 = flock_to_center(a);
+			Rigidbody2D rb = a.GetComponent<Rigidbody2D> ();
+
+			rb.velocity = a.GetComponent<Rigidbody2D>().velocity + strength1 + strength2 + strength3;
+			if (rb.velocity.magnitude > maxSpeed) {
+				rb.velocity = maxSpeed * rb.velocity.normalized;
+			}
         }
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		mousePosition.z = 0;
+		instantaneousVelocity = mousePosition - transform.position;
+		transform.position = mousePosition;
 
 	}
     public Vector2 avoid_collisions(GameObject b) {
-		float strength = Mathf.Min (seperationConstant / Vector3.Distance (transform.position, b.transform.position), maxSpeed);
-
-		return strength * (transform.position - b.transform.position).normalized;
+		Vector3 seperation = Vector2.zero;
+		foreach (GameObject a in flock){
+			
+			if (a != b) {
+				float strength = Mathf.Min (seperationConstant / Vector3.Distance (a.transform.position, b.transform.position), maxSpeed);
+				seperation += strength * (b.transform.position - a.transform.position).normalized;
+			}
+		}
+		
+		float strengthFromLeader = Mathf.Min (seperationConstant / Vector3.Distance (transform.position, b.transform.position), maxSpeed);
+		seperation += strengthFromLeader * (b.transform.position - transform.position).normalized;
+		return new Vector2(seperation.x, seperation.y);
 
     }
     public Vector2 match_velocity(GameObject b)
     {
-        return new Vector2(0, 0);
+		
+
+		return instantaneousVelocity;
 
     }
     public Vector2 flock_to_center(GameObject b)
     {
-        return new Vector2(0, 0);
+		
+		return (transform.position - b.transform.position);
 
     }
     public void coneCheck(GameObject b) {
---
+++ Assets/Scripts/SpawnFlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFlock : MonoBehaviour {
+	public int flockSize;
+	public GameObject flockPrefab;
+	public float initialSeperation;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < flockSize; i++){
+			GameObject flockInstance = Instantiate (flockPrefab);
+			Vector3 position = Vector3.zero;
+			position.x = i * initialSeperation;
+			flockInstance.transform.position = position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
