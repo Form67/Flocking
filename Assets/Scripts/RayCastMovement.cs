3c24c4ac9016459c4ad6fba83a96b32339855c2f
Sam Gould <drmcman67@yahoo.com>
2018-10-11 09:18:39 -0400

    Finished Code for problem 3

--- Assets/Scripts/RayCastMovement.cs
+++ Assets/Scripts/RayCastMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RayCastMovement : MonoBehaviour {
     //Using arbitration to deal with corner trap
+    public GameObject[] path;
     public float arbitrationCooldown;
     public float avoidDistance;
     public int arbitrationWinner;
@@ -11,8 +12,12 @@ public class RayCastMovement : MonoBehaviour {
     public float raycastAngle;
     public Vector2 target;
     public float raycastDistance;
-
-    
+    public float max_acc;
+    public float curr_speed;
+    public float max_speed;
+    public float linear_acc;
+    public bool arrived;
+    public int currentIndex;
     public Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
@@ -54,11 +59,55 @@ public class RayCastMovement : MonoBehaviour {
         }
 
     }
-    public void Seek() {
+    public void Seek() 
+        {
+            linear_acc = Vector3.Distance(transform.position, target);
+            //go to object plus where it will be in a second (pursue);
+            var magnitude = target- new Vector2(transform.position.x, transform.position.y);
+            magnitude = magnitude.normalized;
+            transform.up = magnitude;
+            if (linear_acc > max_acc)
+            {
+                linear_acc = max_acc;
+            }
+            curr_speed += linear_acc;
+            if (curr_speed > max_speed)
+            {
+                curr_speed = max_speed;
+            }
+            GetComponent<Rigidbody2D>().velocity = (magnitude * curr_speed);
 
-    }
+        }
 
-    public void Pathfind() {
-
+    public void Pathfind()
+    {
+        if (!arrived)// seeks to each node on path
+        {
+            linear_acc = Vector3.Distance(transform.position, path[currentIndex].transform.position);
+            var magnitude = -transform.position + path[currentIndex].transform.position;
+            magnitude = magnitude.normalized;
+            if (linear_acc > max_acc)
+            {
+                linear_acc = max_acc;
+            }
+            curr_speed += linear_acc;
+            if (curr_speed > max_speed)
+            {
+                curr_speed = max_speed;
+            }
+            GetComponent<Rigidbody2D>().velocity = (magnitude * curr_speed);
+            if (Vector3.Distance(this.transform.position, path[currentIndex].transform.position) < .5f)
+            {
+                if (currentIndex < path.Length - 1)
+                {
+                    currentIndex++;
+                    transform.up = path[currentIndex].transform.position - transform.position;
+                }
+                else
+                {
+                    arrived = true;
+                }
+            }
+        }
     }
 }
